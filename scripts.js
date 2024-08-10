e6bdf7bfa7367f38493b905745df9d0a12b358ed
Spaco1993 <obinnaspaco@gmail.com>
2024-08-10 15:01:50 +0200

    update

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@ const chatlog = document.querySelector("#chat-log");
 
 
 let userMessage;
+const API_KEY = "";
+
 
 const createChatLi = (message, className) => {
     // create a chat <li> element with passed message and className
@@ -19,8 +21,28 @@ const createChatLi = (message, className) => {
     return chatLi;
 }
 
-const generateResponse = () => {
-    const API_URL = "";
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = incomingChatLi.querySelector("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+           "Content-Type":  "application/json",
+           "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
+
+    // Send POST request to API, get response
+    fetch (API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        messageElement.textContent = "Sorry Something went wrong. Please try again.";
+    })
 }
 
 const handleChat = () => {
@@ -29,14 +51,17 @@ const handleChat = () => {
 
     // Append the users message to the chatlog
     chatlog.appendChild(createChatLi(userMessage, "outgoing"));
+    chatlog.scrollTo(0, chatlog.scrollHeight);
 
     // Clear the input after sending the message
     chatInput.value = "";
 
     // Display "Let me check..." message while user waiting for response
     setTimeout(() => {
-        chatlog.appendChild(createChatLi("Let me check...", "incoming"));
-        generateResponse();
+        const incomingChatLi = createChatLi("Let me check...", "incoming")
+        chatlog.appendChild(incomingChatLi);
+        chatlog.scrollTo(0, chatlog.scrollHeight);
+        generateResponse(incomingChatLi);
     }, 600);
 }
 
